440741e0bc09e0ab4e17476771d09e408062588a
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-12-16 22:33:14 +0500

    Update

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Link } from "gatsby";
+import Header from "../components/Header";
 
 export default function About() {
   return (
     <div>
+      <Header title="Header from about" />
       <h1>About Page</h1>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import { Link } from "gatsby";
 import React from "react";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
+      <Header title="Header from index" />
       <h1>Home Page</h1>
       <br />
       <Link to="/about">About</Link>
