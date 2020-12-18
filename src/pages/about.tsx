523376a3720b98e0c18626009924d34b4e1c5404
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-12-18 21:56:11 +0500

    Updating

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Layout from "../components/Layout";
+import styles from "./about.module.css";
 
 export default function About() {
   return (
     <Layout headerTitle="Header from about">
-      <h1>About Page</h1>
+      <div className={styles.title}>
+        <h1>About Page</h1>
+      </div>
     </Layout>
   );
 }
