9e424d57fb5cabd26c5a3b7ba66f918d1a2a1d50
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-12-20 00:10:39 +0500

    Updating

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -3,12 +3,18 @@ import { Router } from "@reach/router";
 import Layout from "../components/Layout";
 import Client1 from "../components/Client1";
 import Client2 from "../components/Client2";
+import { Link } from "gatsby";
 
 const App = () => {
   return (
     <Layout headerTitle="App Header">
+      <div>
+        <Link to="/app/client1">Client 1</Link>
+        <br />
+        <Link to="/app/client2">Client 2</Link>
+      </div>
       <Router basepath="/app">
-        <Client1 path="/" />
+        <Client1 path="/client1" />
         <Client2 path="/client2" />
       </Router>
     </Layout>
