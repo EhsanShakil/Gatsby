5f972ac1dd9ee1c2ed907df373e370c6fb95f79a
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-12-20 22:20:54 +0500

    Adding material ui

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -3,12 +3,16 @@ import { Router } from "@reach/router";
 import Layout from "../components/Layout";
 import Client1 from "../components/Client1";
 import Client2 from "../components/Client2";
-import { Link } from "gatsby";
+import { Link, navigate } from "gatsby";
+import Button from '@material-ui/core/Button'
 
 const App = () => {
   return (
     <Layout headerTitle="App Header">
       <div>
+        <Button variant="contained" color="primary" onClick={() => navigate("/app/client1")}>Client 1</Button>{" "}
+        <Button variant="contained" color="primary" onClick={() => navigate("/app/client2") }>Client 2</Button> 
+        <br />
         <Link to="/app/client1">Client 1</Link>
         <br />
         <Link to="/app/client2">Client 2</Link>
