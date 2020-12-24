935264d7215cff8ee213f53af4ca7ffb90209c95
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-12-24 20:55:24 +0500

    Updating

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,15 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 module.exports = {
   plugins: [
     "gatsby-plugin-material-ui",
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `7a84f7bwrq7e`,
-        accessToken: `fVteIsLlqFLa0ibNLtAar8RvuSPq7sc09Pg5_DkUQ9M`,
+        spaceId: process.env.SPACE_ID,
+        accessToken: process.env.ACCESS_TOKEN,
       },
     },
   ],
--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 export default ({ pageContext }) => {
   return (
     <div>
-      <div>{documentToReactComponents(pageContext.data, pageContext.contentful_id)}</div>
+      <div>{(pageContext.data.content.raw)}</div>
     </div>
   );
 };
